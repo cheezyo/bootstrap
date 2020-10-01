045f938ae874bfad87ddbac6d183bb89fed22a1b
Cezar Johansson <cezarsincan@gmail.com>
2020-10-01 19:40:02 +0200

    DF

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,10 +5,13 @@
 import '../stylesheets/application'
 import './bootstrap_custom.js'
 import './modal.js'
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("chartkick")
+require("chart.js")
 
 
 
