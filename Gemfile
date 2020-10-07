3097bf4f72819da9646eba380ac4204ce5fa29ca
Cezar Johansson <cezarsincan@gmail.com>
2020-10-07 14:05:25 +0200

    dF

--- Gemfile
+++ Gemfile
@@ -35,7 +35,7 @@ gem 'jbuilder', '~> 2.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 gem 'chartkick'
-gem 'carrierwave', '~> 1.0'
+gem 'carrierwave', '~> 2.0'
 gem 'fog-aws'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,15 @@
 CarrierWave.configure do |config|
-	config.fog_provider = "fog/aws"
-	config.fog_credentials = { 
-		provider: 				'AWS',
-		aws_access_key_id: 		ENV['S3_KEY'],
-		aws_secret_access_key: 	ENV['S3_SECRET'],
-		region: 				ENV['S3_REGION'],
-	}
-	config.fog_directory = ENV['S3_BUCKET']
+  config.fog_credentials = {
+    :provider               => 'AWS',                        # required
+    :aws_access_key_id      => ENV['S3_KEY'],                        # required
+    :aws_secret_access_key  => ENV['S3_SECRET'],                        # required
+    :region                 => 'eu-central-1',                  # optional, defaults to 'us-east-1'
+    :endpoint => 'https://s3-eu-central-1.amazonaws.com'
+  }
+  config.fog_directory  = 'stk-server'                     # required
+  config.fog_public     = false                                   # optional, defaults to true
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
+
+#'AKIAIHMLQEQDE2LFTJ5A'
+#'K4A0nyLMF9Y2n0y1d71cvVCCGwfCCl0VUt2ru+P8'
