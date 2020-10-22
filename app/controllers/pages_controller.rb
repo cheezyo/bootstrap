4e206adfad2ab7daf41df0e584155963c03b73c4
Cezar Johansson <cezarsincan@gmail.com>
2020-10-22 21:14:04 +0200

    df

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,9 +10,11 @@ class PagesController < ApplicationController
 
 	def task_report
 		@tasks = Array.new
-		Task.group_by{|x| x.created_at.strftime("%Y-%m-%d")}.each do |t|
+		Task.all.group_by{|x| x.created_at.strftime("%Y-%m-%d")}.each do |t|
 			@tasks << [t[0], t[1].count]
 		end
+
+		@json_matches = HTTParty.get("https://agw-prod.myutr.com/v1/player/835762/results?year=last&Type=singles", headers: {"Authorization" => "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNZW1iZXJJZCI6IjEyOTQxMCIsImVtYWlsIjoiY2V6YXJzaW5jYW5AaG90bWFpbC5jb20iLCJWZXJzaW9uIjoiMSIsIkRldmljZUxvZ2luSWQiOiI0NzMyNTQ2IiwibmJmIjoxNjAxNDUyNzIzLCJleHAiOjE2MDQwNDQ3MjMsImlhdCI6MTYwMTQ1MjcyM30.phM9zNHzbAfqMQtcivh90nB6nfCeHWPGFbsCoQil6AA"})
 	end
 
 
